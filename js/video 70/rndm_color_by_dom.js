3e24176b21665810cb82085f7218ac71f1b473fb
Praveen Maurya <praveenattanagar186249@gmail.com>
2026-03-01 19:14:49 +0530

    Dynamic website Builder

--- js/video 70/rndm_color_by_dom.js
+++ js/video 70/rndm_color_by_dom.js
@@ -1,9 +1,40 @@
+function getRandomColor(){
+    // a+r(b-a)
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
 
 function color_box() {
-    let colors = ["yellow", "pink", "green", "red", "blue", "aqua", "violet", "orange", "maroon", "aquamarine"];
+    // let colors = ["yellow", "pink", "green", "red", "blue", "aqua", "violet", "orange", "maroon", "aquamarine"];
+
+
     document.querySelectorAll(".box").forEach(element => {
-        element.style.backgroundColor = colors[Math.floor(Math.random() * 10)];
+        // element.style.backgroundColor = colors[Math.floor(Math.random() * 10)];
+        element.style.backgroundColor = getRandomColor();
+        element.style.color = getRandomColor();
     })
 }
 
 color_box();
+
+// Harry bhai ka solution
+
+
+// console.log("Script.js initializing")
+// // let boxes = document.getElementsByClassName("box")
+// let boxes = document.querySelector(".container").children 
+
+
+// function getRandomColor(){
+//     // a+r(b-a)
+//     let val1 = Math.ceil(0 + Math.random()* 255);
+//     let val2 = Math.ceil(0 + Math.random()* 255);
+//     let val3 = Math.ceil(0 + Math.random()* 255);
+//     return `rgb(${val1}, ${val2}, ${val3})`
+// }
+// Array.from(boxes).forEach(e=>{
+//     e.style.backgroundColor = getRandomColor()
+//     e.style.color = getRandomColor()
+// })
---
+++ js/video 73/script.js
@@ -0,0 +1,44 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+
+    function views_convertor(views) {
+        if (views >= 1000000) {
+            return views / 1000000 + "M"
+        }
+        else if (views >= 1000 && views < 1000000) {
+            return views / 1000 + "K"
+        }
+        return views
+    }
+
+    let main = document.querySelector(".container");
+    main.insertAdjacentHTML("beforeend", `<div class="card">
+            <div class="card-img">
+                <img src="${thumbnail}"
+                    width="200px" alt="">
+                <p>${duration}</p>
+            </div>
+            <div class="card-content">
+                <h3>
+                    ${title}
+                </h3>
+                <div class="card-spects">
+                    <p>${cName}</p>
+                    <span>
+                        <h3>.</h3>
+                    </span>
+                    <p>${views_convertor(views)} Views</p>
+                    <span>
+                        <h3>.</h3>
+                    </span>
+                    <p>${monthsOld} months ago</p>
+                </div>
+            </div>
+        </div>`);
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 56000000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
+
+createCard("kalu prasad", "lalu prasad", 560000, 8, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
